5fa5b3129e8d5bd6a2a5e95ebc6c341dc9fd4003
SeamusMGB <seamusmcb@gmail.com>
2022-01-02 09:34:25 +0000

    Add javascript to display result

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,13 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
@@ -23,4 +25,29 @@ function generateComputerChoice() {
         computerChoice = 'scissors'
     }
     computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'Tie!'
+    }  
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'Victory!'
+    }  
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'Loss!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'Victory!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'Loss!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'Victory!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'Loss!'
+    }
+    resultDisplay.innerHTML = result
 }
