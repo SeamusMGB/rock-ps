07adb34bc483d68e2a59706ada1e524b7a9fda51
SeamusMGB <seamusmcb@gmail.com>
2022-01-02 09:25:09 +0000

    Add javascript to generate random value for computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,20 @@ let userChoice
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) 
+    
+    if (randomNumber === 0) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 1) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
